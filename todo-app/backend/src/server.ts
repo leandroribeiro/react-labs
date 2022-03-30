a1d0d445d8d915ea992dd9a6107048d459aec412
Leandro Ribeiro <github@leandroribeiro.com>
2022-03-29 21:10:53 -0300

    build: configura pm2

--- todo-app/backend/src/server.ts
+++ todo-app/backend/src/server.ts
@@ -1,11 +1,11 @@
 
-import express from 'express'
+import express, { Express } from 'express'
 import bodyParser from 'body-parser'
 import { cors } from './config/cors'
 import routes from './routes';
 
-const server = express()
-const port = 3003;
+const server: Express = express()
+const port = process.env.PORT || 3003;
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(express.json())
@@ -14,7 +14,7 @@ server.use(routes)
 
 server.listen(port, () =>
     console.log(`
-🚀 Server ready at: http://localhost:${port}`)
+🚀 Server ready at: http://localhost:${port}!`)
 )
 
-    
+

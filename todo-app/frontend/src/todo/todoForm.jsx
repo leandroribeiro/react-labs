70cfdee4c7ad7d23c919e8e0563503152c354e29
Leandro Ribeiro <github@leandroribeiro.com>
2022-04-30 22:26:35 -0300

    feat: teclas de atalho

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -3,6 +3,13 @@ import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
 export default function TodoForm(props) {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        } else if (e.key === 'Escape') {
+            props.handleClear();
+        }
+    }
 
     return (
         <div role="form" className="todoForm">
@@ -13,7 +20,8 @@ export default function TodoForm(props) {
                     className="form-control"
                     placeholder="Adicione uma tarefa"
                     value={props.description}
-                    onChange={props.handleChange} />
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}/>
             </Grid>
             <Grid cols="12 3 2">
                 <IconButton
